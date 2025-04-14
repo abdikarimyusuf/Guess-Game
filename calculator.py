ecf989897d05754a8396e46a379b8b5f6d54ff99
abdikarim yusuf <abdikarimyusuf6@gmail.com>
2025-04-15 00:11:06 +0100

    first commit -all files to rep

---
+++ calculator.py
@@ -0,0 +1,41 @@
+import random
+
+ran_1=random.randint(1,100)
+while True:
+ try:
+
+  user=input("guess num 1-100:")
+  guess =int(user)
+
+  
+
+
+
+  if guess < ran_1:
+   print ("too low")
+  elif guess > ran_1:
+
+   print ("too high")
+  else:
+   print ("well done ")
+   break
+ except:
+  print ("input a number")
+
+    
+    
+
+
+
+ 
+
+     
+
+
+  
+  
+
+
+
+
+
