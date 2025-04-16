c91cb6cb0f504d6911092dacdd1245c7d6536cf6
abdikarim yusuf <abdikarimyusuf6@gmail.com>
2025-04-17 00:04:44 +0100

    added functions

--- rockpapersci.py
+++ rockpapersci.py
@@ -3,35 +3,60 @@ import random
 choices = ['r','p','s']
 
 emojis = {'r':'🪨','p':'🔖','s':'✂'}
-while True:
-
+def user_choice():
+  while True:
     user_choice= input("rock,paper or sciccors? (r/p/s):" ).lower()
-    if user_choice not in choices:
-     print ('invalid input')
-     continue
+    if user_choice in choices:
+        return user_choice
+    else:
+        print ("invalid input")
+        continue
 
-    computer_choice=random.choice(choices)
+    
+def user_display(user_choce,computer_choice):
+        print (f'you chose:{emojis[user_choce]}')
+        print (f'computer chose:{emojis[computer_choice]}')
 
-    print (f'you chose:{emojis[user_choice]}')
-    print (f'computer chose:{emojis[computer_choice]}')
+def determine_winner(user_choice,computer_choice):
+   
+        if user_choice == computer_choice:
+            print('tie')
+
+        elif user_choice == 'r' and computer_choice == 'p':
+            print('computer won')
+        elif user_choice == 'p' and computer_choice == 'r':
+            print('you won')
+        elif user_choice == 's' and computer_choice == 'p':
+            print('you won')
+        elif user_choice == 'p' and computer_choice == 's':
+            print('computer won')
+        elif user_choice == 's' and computer_choice == 'r':
+            print('computer won')
+        elif user_choice == 'r' and computer_choice == 's':
+            print('you won')
+        
+
+        
+       
+
+def play_game():       
+    while True:
+
+        player=user_choice()
+
+            
+
+        computer_choice=random.choice(choices)
+        
+        user_display(player,computer_choice)
+
+        determine_winner(player,computer_choice)
 
 
-    if user_choice == computer_choice:
-        print('tie')
+        should_continue=input('do you want to continue: (y/n):').lower()
+        if should_continue =='n':
+                break
+            
 
-    elif user_choice == 'r' and computer_choice == 'p':
-        print('computer won')
-    elif user_choice == 'p' and computer_choice == 'r':
-        print('you won')
-    elif user_choice == 's' and computer_choice == 'p':
-        print('you won')
-    elif user_choice == 'p' and computer_choice == 's':
-        print('computer won')
-    elif user_choice == 's' and computer_choice == 'r':
-        print('computer won')
-    elif user_choice == 'r' and computer_choice == 's':
-        print('you won')
-
-    should_continue=input('do you want to continue: (y/n):').lower()
-    if should_continue =='n':
-        break
+play_game()
+        
